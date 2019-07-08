bcb5265de3edec74b3c53559f7933ca9ac660d9d
Nickolas.Castle <ncastle@uvm.edu>
2019-07-08 09:21:34 -0400

    beginning of class 7/8 update

--- main.js
+++ main.js
@@ -6,8 +6,19 @@ const toggleNav = () => {
   burger.addEventListener('click', () => {
     console.log('burger clicked');
     nav.classList.toggle('nav-open');
-  })
+  });
 }
+
+// do this later
+// const makeButton = () => {
+//   const nextButton = document.getElementById('next-button');
+//   nextButton.addEventListener('click', () => {
+//     console.log('next button clicked!');
+//     console.log({nextButton});
+//     nextButton.attributes.onClick = 'window.location.href="#about-top"';
+//   });
+// }
 
 // start the function
 toggleNav();
+// makeButton();

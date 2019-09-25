f03c8d7726d7ccec04fd7744f2dc20d236b0cc14
Nickolas.Castle <ncastle@uvm.edu>
2019-09-25 17:36:03 -0400

    Header and Nav Bar redesigned
        - changed color
        - 'welcome' changed to 'N.Castle Development'
        - list items / nav links are button-like on hover
        - burger changed to a left arrow when closed and right arrow when
        open
        - nav links slide in from the right in a row instead of top when in
        mobile view

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const toggleNav = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('nav');
   const navLinks = document.querySelectorAll('nav li');
+  const burgerDivs = document.querySelectorAll('.burger > div');
 
   burger.addEventListener('click', () => {
     console.log('burger clicked');
     nav.classList.toggle('nav-open');
+    burger.classList.toggle('toggle');
   });
 }
 
